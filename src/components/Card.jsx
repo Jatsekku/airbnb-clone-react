55a9da94f38bcd5e7373aeb1667428a1215b3158
Jatsekku <jatsekku2@gmail.com>
2022-11-19 14:55:01 +0100

    App/Components: Add badge to card component
    This commit provide badge displayed on top-left corner of the card's
    image. It is provided as conditional rendering:
    - "SOLD OUT" badge when there no free spots
    - "ONLINE" badge when location is set to online

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,23 @@ import './Card.css'
 import star from '../assets/star.png'
 
 export default function Card(props) {
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card--container">
-            <img className="card--image" src={props.img}/>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img className="card--image" src={props.item.coverImg}/>
             <div className="card-stats">
                 <img className="card--star" src={star}/>
-                <span>{props.rating}</span>
-                <span className="card--gray">({props.reviewCount}) • </span>
-                <span className="card--gray">{props.country}</span>
-                <p className="card--title">{props.title}</p>
-                <p> <span className="card--price">From ${props.price}</span> / person </p>
+                <span>{props.item.stats.rating}</span>
+                <span className="card--gray">({props.item.stats.reviewCount}) • </span>
+                <span className="card--gray">{props.item.location}</span>
+                <p className="card--title">{props.item.title}</p>
+                <p> <span className="card--price">From ${props.item.price}</span> / person </p>
             </div>
         </div>
     )
